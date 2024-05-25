abb0176b449295ee33d104f3cc1e0e16fee060c7
Egor-kalash <66547519+Egor-kalash@users.noreply.github.com>
2024-05-25 16:24:56 -0400

    jh

--- outlet/JS/start_page.js
+++ outlet/JS/start_page.js
@@ -1,36 +1,66 @@
+
+
+const body = document.getElementsByTagName("body");
 const words = document.getElementById("words").children;
-console.log(words);
+let windowHeight = window.innerHeight;
+let windowWidth = window.innerWidth;
+const timer = ms => new Promise(res => setTimeout(res, ms));
 
 
+window.onresize = () => {
+  windowHeight = window.innerHeight;
+  windowWidth = window.innerWidth;
+};
+
+let a = Math.floor(windowWidth / 170);
+let b = Math.floor((windowHeight)/ 18);
+let numberOfTexts = a * b;
+
+createAppearingText(numberOfTexts);
 // redu in react //
 function createAppearingText(n){
   const container = document.getElementById("words");
-  const text = "<div><span>Make Your Voice Sound</span></div>".repeat(n);
+  const text = "<div><span>The Voice</span></div>".repeat(n);
   container.innerHTML = text;
   //         <div><span>Make Your Voice Sound</span></div>
 }
 
 
-function hideUnnecessery(){
+async function hideUnnecessery(){
   for (let i = 0; i < words.length; i++) {
     const num = Math.floor(Math.random() * 10);
-    if (num >= 4) {
-      words[i].children[0].style.display = 'block';
-      
-    } 
-    else {
-      words[i].children[0].style.display = 'none';
-    }
+      if (num >= 5) {
+        words[i].children[0].style.opacity = '0';
+        
+      } 
+      else {
+        words[i].children[0].style.opacity = '.5';
+      }
+    await timer(0.5);
   }
 }
 
 
+function hideInitial(){
+  for (let i = 0; i < words.length; i++) {
+    const num = Math.floor(Math.random() * 10);
+      if (num >= 5) {
+        words[i].children[0].style.opacity = '0';
+        
+      } 
+      else {
+        words[i].children[0].style.opacity = '.5';
+      }
+  }
+}
+
 function rotateScheam(){
+  hideInitial();
   setInterval(() => {
     hideUnnecessery();
-  }, 1000)
+  }, words.length)
 }
-/* Start the Functions */
-createAppearingText(100);
-hideUnnecessery();
+/* Start the Functionsq */
+
 rotateScheam();
+
